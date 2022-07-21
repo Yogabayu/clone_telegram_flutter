87db7027ad70df4ad7ab1a9499b51221d7367432
Yoga Bayu AP <yogabayusbi@gmail.com>
2022-07-21 22:58:44 +0700

    update search bar

---
+++ lib/model/media.dart
@@ -0,0 +1,34 @@
+class PhotoMedia {
+  final String image;
+  final String title;
+  PhotoMedia({required this.image, required this.title});
+}
+
+List<PhotoMedia> get itemMedia => _items;
+
+List<PhotoMedia> _items = [
+  PhotoMedia(image: "https://picsum.photos/200/300?random=", title: "img1"),
+  PhotoMedia(image: "https://picsum.photos/200/300?random=", title: "img2"),
+  PhotoMedia(image: "https://picsum.photos/200/300?random=", title: "img3"),
+  PhotoMedia(image: "https://picsum.photos/200/300?random=", title: "img4"),
+  PhotoMedia(image: "https://picsum.photos/200/300?random=", title: "img5"),
+  PhotoMedia(image: "https://picsum.photos/200/300?random=", title: "img6"),
+  PhotoMedia(image: "https://picsum.photos/200/300?random=", title: "img7"),
+  PhotoMedia(image: "https://picsum.photos/200/300?random=", title: "img8"),
+  PhotoMedia(image: "https://picsum.photos/200/300?random=", title: "img9"),
+  PhotoMedia(image: "https://picsum.photos/200/300?random=", title: "img10"),
+  PhotoMedia(image: "https://picsum.photos/200/300?random=", title: "img11"),
+  PhotoMedia(image: "https://picsum.photos/200/300?random=", title: "img12"),
+  PhotoMedia(image: "https://picsum.photos/200/300?random=", title: "img1"),
+  PhotoMedia(image: "https://picsum.photos/200/300?random=", title: "img2"),
+  PhotoMedia(image: "https://picsum.photos/200/300?random=", title: "img3"),
+  PhotoMedia(image: "https://picsum.photos/200/300?random=", title: "img4"),
+  PhotoMedia(image: "https://picsum.photos/200/300?random=", title: "img5"),
+  PhotoMedia(image: "https://picsum.photos/200/300?random=", title: "img6"),
+  PhotoMedia(image: "https://picsum.photos/200/300?random=", title: "img7"),
+  PhotoMedia(image: "https://picsum.photos/200/300?random=", title: "img8"),
+  PhotoMedia(image: "https://picsum.photos/200/300?random=", title: "img9"),
+  PhotoMedia(image: "https://picsum.photos/200/300?random=", title: "img10"),
+  PhotoMedia(image: "https://picsum.photos/200/300?random=", title: "img11"),
+  PhotoMedia(image: "https://picsum.photos/200/300?random=", title: "img12"),
+];
--- lib/model/pengaturan.dart
+++ lib/model/pengaturan.dart
@@ -1,7 +1,6 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-//TODO pengaturan model blm selsai
 class PengaturanMenu {
   final String id;
   final IconData ikon;
@@ -14,5 +13,40 @@ List<PengaturanMenu> dummyPengaturan = [
     id: "1",
     ikon: FontAwesomeIcons.bell,
     title: "Notifikasi dan Suara",
+  ),
+  PengaturanMenu(
+    id: "2",
+    ikon: Icons.lock_outline,
+    title: "Privasi dan Keamanan",
+  ),
+  PengaturanMenu(
+    id: "3",
+    ikon: Icons.pie_chart_outline,
+    title: "Data dan Penyimpanan",
+  ),
+  PengaturanMenu(
+    id: "4",
+    ikon: FontAwesomeIcons.comment,
+    title: "Pengaturan Obrolan",
+  ),
+  PengaturanMenu(
+    id: "5",
+    ikon: Icons.emoji_emotions_outlined,
+    title: "Stiker dan Emoji",
+  ),
+  PengaturanMenu(
+    id: "6",
+    ikon: Icons.folder_open_outlined,
+    title: "Folder Obrolan",
+  ),
+  PengaturanMenu(
+    id: "7",
+    ikon: Icons.devices,
+    title: "Perangkat",
+  ),
+  PengaturanMenu(
+    id: "8",
+    ikon: FontAwesomeIcons.globe,
+    title: "Bahasa",
   ),
 ];
