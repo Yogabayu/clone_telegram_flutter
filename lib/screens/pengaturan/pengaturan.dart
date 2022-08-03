ad974f812ca6f66a91c43caac7bcc33e34b31865
Yoga Bayu AP <yogabayusbi@gmail.com>
2022-08-03 22:18:31 +0700

    fix tampilan pengaturan

--- lib/screens/pengaturan/pengaturan.dart
+++ lib/screens/pengaturan/pengaturan.dart
@@ -1,121 +1,256 @@
+// ignore_for_file: unused_element
+
 import 'package:clone_telegram/model/pengaturan.dart';
 import 'package:clone_telegram/provider/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:provider/provider.dart';
-//TODO add sliverappbar
 
+//TODO blm selesai penggilan
 class Pengaturan extends StatefulWidget {
-  const Pengaturan({Key? key}) : super(key: key);
-
   @override
-  _PengaturanState createState() => _PengaturanState();
+  State<Pengaturan> createState() => _PengaturanState();
 }
 
 class _PengaturanState extends State<Pengaturan> {
+  ScrollController? _scrollController;
+
+  bool lastStatus = true;
+
+  double height = 200;
+
+  void _scrollListener() {
+    if (_isShrink != lastStatus) {
+      setState(() {
+        lastStatus = _isShrink;
+      });
+    }
+  }
+
+  bool get _isShrink {
+    return _scrollController != null &&
+        _scrollController!.hasClients &&
+        _scrollController!.offset > (height - kToolbarHeight);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController = ScrollController()..addListener(_scrollListener);
+  }
+
+  @override
+  void dispose() {
+    _scrollController?.removeListener(_scrollListener);
+    _scrollController?.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     final width = MediaQuery.of(context).size.width;
     final height = MediaQuery.of(context).size.height;
-    return Consumer<ThemeModel>(
-      builder: (context, ThemeModel themeNotifier, child) {
-        return Scaffold(
-          // backgroundColor: Colors.black,
-          body: Container(
-            width: width,
-            height: height,
-            child: ListView(
-              physics: ClampingScrollPhysics(),
-              children: [
-                Stack(
-                  children: [
-                    Column(
-                      children: [
-                        _header(context),
-                        _akun(context),
-                        Divider(color: Colors.transparent),
-                        _pengaturan(context),
-                        Divider(color: Colors.transparent),
-                        _premium(context),
-                        Divider(color: Colors.transparent),
-                        _bantuan(context),
-                        SizedBox(
-                          height: 10,
-                        ),
-                        Container(
-                          color: Colors.transparent,
-                          child: Text(
-                            "Telegram untuk Android v8.8.4 (2711) store bundled arm64-v8a",
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                              color: Colors.grey,
-                            ),
-                          ),
-                        ),
-                        SizedBox(
-                          height: 50,
-                        )
-                      ],
+    return Scaffold(
+      body: Stack(
+        children: [
+          NestedScrollView(
+            controller: _scrollController,
+            headerSliverBuilder: (context, innerBoxIsScrolled) {
+              return [
+                SliverAppBar(
+                  leading: IconButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    icon: Icon(
+                      Icons.arrow_back,
+                      color: Colors.white,
                     ),
-                    Positioned(
-                      top: width * 0.28,
-                      left: width * 0.8,
-                      child: Container(
-                        decoration: BoxDecoration(
-                          // border: Border.all(color: Colors.grey),
-                          borderRadius: BorderRadius.circular(width * 0.8),
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.grey.withOpacity(0.2),
-                              blurRadius: 0.1,
-                              offset:
-                                  Offset(0, 3), // changes position of shadow
+                  ),
+                  elevation: 0,
+                  backgroundColor: Color.fromARGB(255, 86, 163, 226),
+                  pinned: true,
+                  expandedHeight: 212,
+                  flexibleSpace: FlexibleSpaceBar(
+                    collapseMode: CollapseMode.parallax,
+                    titlePadding: EdgeInsets.only(bottom: 15, left: 40),
+                    title: _isShrink
+                        ? Padding(
+                            padding: const EdgeInsets.only(top: 25.0, left: 0),
+                            child: ListTile(
+                              leading: CircleAvatar(
+                                backgroundImage: NetworkImage(
+                                    'https://picsum.photos/seed/girl/200/300'),
+                              ),
+                              title: Text(
+                                "Saya Aslinya Ultraman",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              subtitle: Text(
+                                "online",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                ),
+                              ),
                             ),
-                          ],
-                        ),
-                        child: CircleAvatar(
-                          radius: 27,
-                          backgroundColor: Colors.white,
-                          child: Icon(
-                            Icons.camera_alt_outlined,
-                            color: Colors.grey.withOpacity(0.6),
-                            size: 30,
-                            // color: Colors.black,
-                          ),
+                          )
+                        : null,
+                    background: Container(
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          fit: BoxFit.cover,
+                          image: NetworkImage(
+                              'https://picsum.photos/seed/girl/200/300'),
                         ),
                       ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.only(
+                              left: 30,
+                              bottom: 10,
+                            ),
+                            child: Text(
+                              "Saya Aslinya Ultramen",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 25,
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(left: 30, bottom: 30),
+                            child: Text(
+                              "Online",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 15,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
-                  ],
+                  ),
+                  actions: _isShrink
+                      ? [
+                          Padding(
+                            padding: const EdgeInsets.only(left: 8, right: 5),
+                            child: Row(
+                              children: [
+                                IconButton(
+                                  onPressed: () {},
+                                  icon: Icon(
+                                    Icons.search,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                IconButton(
+                                  onPressed: () {},
+                                  icon: Icon(
+                                    Icons.more_vert_rounded,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ]
+                      : [
+                          Padding(
+                            padding: const EdgeInsets.only(left: 8, right: 5),
+                            child: Row(
+                              children: [
+                                IconButton(
+                                  onPressed: () {},
+                                  icon: Icon(
+                                    Icons.qr_code,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                IconButton(
+                                  onPressed: () {},
+                                  icon: Icon(
+                                    Icons.more_vert_rounded,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
                 ),
-              ],
+              ];
+            },
+            body: Container(
+              padding: EdgeInsets.zero,
+              width: width,
+              height: height,
+              child: ListView(
+                padding: EdgeInsets.zero,
+                children: [
+                  _akun(context),
+                  Divider(color: Colors.transparent),
+                  _pengaturan(context),
+                  Divider(color: Colors.transparent),
+                  _premium(context),
+                  Divider(color: Colors.transparent),
+                  _bantuan(context),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Container(
+                    color: Colors.transparent,
+                    child: Text(
+                      "Telegram untuk Android v8.8.4 (2711) store bundled arm64-v8a",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 50,
+                  )
+                ],
+              ),
             ),
           ),
-        );
-      },
-    );
-  }
-}
-
-class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
-  final Widget child;
-  final double height;
-
-  CustomAppBar({
-    required this.child,
-    this.height = kToolbarHeight,
-  });
-
-  @override
-  Size get preferredSize => Size.fromHeight(height);
-
-  @override
-  Widget build(BuildContext context) {
-    final width = MediaQuery.of(context).size.width;
-    return Container(
-      padding: EdgeInsets.only(top: width * 0.1),
-      height: preferredSize.height,
-      color: Color.fromARGB(255, 139, 182, 218),
-      child: child,
+          Positioned(
+              top: width * 0.55,
+              left: width * 0.8,
+              child: _isShrink
+                  ? SizedBox()
+                  : Container(
+                      decoration: BoxDecoration(
+                        // border: Border.all(color: Colors.grey),
+                        borderRadius: BorderRadius.circular(width * 0.8),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.2),
+                            blurRadius: 0.1,
+                            offset: Offset(0, 3), // changes position of shadow
+                          ),
+                        ],
+                      ),
+                      child: CircleAvatar(
+                        radius: 27,
+                        backgroundColor: Colors.white,
+                        child: Icon(
+                          Icons.camera_alt_outlined,
+                          color: Colors.grey.withOpacity(0.6),
+                          size: 30,
+                          // color: Colors.black,
+                        ),
+                      ),
+                    )),
+        ],
+      ),
     );
   }
 }
@@ -130,7 +265,7 @@ Widget _akun(context) {
         color: themeNotifier.isDark
             ? Color.fromARGB(255, 29, 31, 31)
             : Colors.white,
-        height: 215,
+        // height: width * 0.53,
         width: width,
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
@@ -292,7 +427,6 @@ Widget _pengaturan(context) {
         color: themeNotifier.isDark
             ? Color.fromARGB(255, 29, 31, 31)
             : Colors.white,
-        height: height * 0.69,
         width: width,
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
