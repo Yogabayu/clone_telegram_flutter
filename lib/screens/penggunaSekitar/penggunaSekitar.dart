8ce606137fd726d5700f40f5764a96ee159f42ef
Yoga Bayu AP <yogabayusbi@gmail.com>
2022-08-10 06:27:00 +0700

    add tofo pengguna?

--- lib/screens/penggunaSekitar/penggunaSekitar.dart
+++ lib/screens/penggunaSekitar/penggunaSekitar.dart
@@ -1,6 +1,11 @@
 import 'package:clone_telegram/provider/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:provider/provider.dart';
+import 'package:lottie/lottie.dart';
+
+bool isShown = false;
 
 //TODO pengguna sekitar belum selesai
 class PenggunaSekitar extends StatefulWidget {
@@ -11,6 +16,21 @@ class PenggunaSekitar extends StatefulWidget {
 }
 
 class _PenggunaSekitarState extends State<PenggunaSekitar> {
+  void _cekLocation() async {
+    isShown = await Permission.locationWhenInUse.serviceStatus.isEnabled;
+    if (isShown) {
+      isShown = true;
+    } else {
+      isShown = false;
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _cekLocation();
+  }
+
   @override
   Widget build(BuildContext context) {
     final double width = MediaQuery.of(context).size.width;
@@ -26,23 +46,103 @@ class _PenggunaSekitarState extends State<PenggunaSekitar> {
               icon: Icon(Icons.arrow_back),
               onPressed: () => Navigator.of(context).pop(),
             ),
-            title: Text(
-              "Pengguna Sekitar",
-              style: TextStyle(
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
-                fontSize: width * 0.05,
-              ),
-            ),
-            actions: [
-              IconButton(
-                onPressed: () {},
-                icon: Icon(Icons.more_vert_rounded),
-              ),
-            ],
           ),
+          body: isShown ? Text("pengguna sekitar") : _containerNot(context),
         );
       },
     );
   }
+}
+
+Widget _containerNot(context) {
+  final double width = MediaQuery.of(context).size.width;
+  final double height = MediaQuery.of(context).size.height;
+  return Container(
+    margin: EdgeInsets.symmetric(horizontal: 40),
+    width: width,
+    height: height,
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(50),
+            color: Colors.blue,
+          ),
+          padding: EdgeInsets.all(10),
+          height: width * 0.2,
+          width: width * 0.2,
+          child: ClipOval(
+            child: Lottie.asset(
+              "assets/pinMap.json",
+              animate: true,
+              alignment: Alignment.center,
+            ),
+          ),
+        ),
+        SizedBox(
+          height: width * 0.08,
+        ),
+        Text(
+          "Pengguna Sekitar",
+          style: TextStyle(
+            fontSize: 20,
+          ),
+        ),
+        SizedBox(
+          height: width * 0.1,
+        ),
+        Text(
+          "Gunakan Bagian ini untuk menambahkan Penggunaan Sekitar & menemukan Grup di sekitar Anda lebih cepat.",
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 15,
+            color: Colors.grey,
+          ),
+        ),
+        SizedBox(
+          height: width * 0.1,
+        ),
+        Text(
+          "Mohon izinkan akses lokasi untuk menggunakan fitur ini",
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 15,
+            color: Colors.grey,
+          ),
+        ),
+        SizedBox(
+          height: width * 0.1,
+        ),
+        Container(
+          width: width * 0.5,
+          child: ElevatedButton(
+            style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all(Colors.blue),
+            ),
+            onPressed: () async {
+              LocationPermission permission;
+              permission = await Geolocator.checkPermission();
+              if (permission == LocationPermission.denied) {
+                permission = await Geolocator.requestPermission();
+                if (permission == LocationPermission.denied) {
+                  isShown = false;
+                }
+              }
+
+              if (permission == LocationPermission.deniedForever) {
+                isShown = false;
+              }
+              if (permission == LocationPermission.always ||
+                  permission == LocationPermission.whileInUse) {
+                isShown = true;
+              }
+            },
+            child: Text("Nyalakan"),
+          ),
+        ),
+      ],
+    ),
+  );
 }
